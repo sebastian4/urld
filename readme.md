81c0ed3743e44d5f27cb2f7035eb672e3438b043
sebastian <sebastianmac@Sebastians-MBP.home>
2015-05-15 22:54:40 -0400

    implementing + readme

---
+++ readme.md
@@ -0,0 +1,24 @@
+urld
+====
+
+encodes and decodes any url
+
+##instructions
+
+```
+urld --encode "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=<html>"
+
+https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=%3Chtml%3E
+
+```
+
+```
+url --decode "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=%3Chtml%3E"
+
+https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=<html>
+
+```
+
+##installation
+
+npm install -g urld
--- urld.js
+++ urld.js
@@ -1,11 +1,27 @@
 #!/usr/bin/env node
 
 var argv = require('yargs').argv;
- 
+var urlencode = require('urlencode2');
+
 if (argv.encode) {
-    console.log('encoding '+argv.encode);
+
+    var parts = argv.encode.split("?");
+
+    if (typeof(parts[1]) == "undefined") {
+
+      console.log(argv.encode);
+
+    } else {
+
+      var part2encoded = urlencode(parts[1]).replace(/%26/g,"&").replace(/%3D/g,"=").replace(/%23/,"#");
+      console.log([parts[0],part2encoded].join("?"));
+
+    }
+
 }
 
 if (argv.decode) {
-    console.log('decoding '+argv.decode);
+
+    console.log( urlencode.decode(argv.decode) );
+
 }
